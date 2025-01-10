11805ee6054a5e8f36023a63f20ab5ebe156fa3c
Dibora Dawit <dawitdibora5106@gmail.com>
2025-01-10 19:29:25 +0200

    search is done

--- src/components/Topic.jsx
+++ src/components/Topic.jsx
@@ -4,6 +4,7 @@ import React, { useEffect, useState } from "react";
 function Topic() {
   const [topics, useTopic] = useState([]);
   const [selectedTopic, setSelectedTopic] = useState();
+  const [search, setSearch] = useState("");
 
   useEffect(() => {
     fetchTopics();
@@ -21,6 +22,10 @@ function Topic() {
     setSelectedTopic(id);
   };
 
+  const filteredTopics = topics.filter((topic) =>
+    topic.name.toLowerCase().includes(search.toLowerCase())
+  );
+
   const colors = [
     "bg-red-500",
     "bg-blue-500",
@@ -35,21 +40,39 @@ function Topic() {
   return (
     <div className="flex flex-col gap-20">
       <div className="text-7xl font-extrabold underline">Topic</div>
+      <div>
+        <input
+          type="text"
+          placeholder="Search ..."
+          name=""
+          className="w-[50vw] bg-white text-black px-10 py-3 rounded-full"
+          id=""
+          onChange={(e) => setSearch(e.target.value)}
+        />
+      </div>
       {selectedTopic && <div> selected topic id is {selectedTopic}</div>}
       <div className="flex flex-wrap gap-4">
-        {topics.map((topic, index) => {
-          return (
-            <a
-              className={`${
-                colors[index % colors.length]
-              } text-black px-5 py-2`}
-              href={`/difficulty/${topic.id}`}
-              //   onClick={() => getTopicID(topic.id)}
-            >
-              {topic.name}
-            </a>
-          );
-        })}
+        {filteredTopics.length ? (
+          filteredTopics.map((topic, index) => {
+            return (
+              <a
+                className={`${
+                  colors[index % colors.length]
+                } text-black px-5 py-2`}
+                href={`/difficulty/${topic.id}`}
+                //   onClick={() => getTopicID(topic.id)}
+              >
+                {topic.name}
+              </a>
+            );
+          })
+        ) : (
+          <div className="flex justify-center w-full">
+            <div className="text-xl font-extrabold animate-pulse">
+              No results found, try with another keyword
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
